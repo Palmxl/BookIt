8e528cdb4a0ccedcd3f076d247807f316558a62f
Juan Martín Sánchez <jmartin_sanchez@javeriana.edu.co>
2024-11-19 18:20:48 -0500

    Main

--- BookIt/src/main/java/com/example/bookit/Main.java
+++ BookIt/src/main/java/com/example/bookit/Main.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("LoginUsuarios.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("LoginUsuarios.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("BookIt - Gestión de reservas y eventos");
         stage.setScene(scene);
         stage.show();
     }
